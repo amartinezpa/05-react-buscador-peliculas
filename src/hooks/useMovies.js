aad7395df32e61212923e0d525005f5dc6cd694b
amartinezpa <amartpa@outlook.com>
2023-06-01 13:01:09 +0200

    uso de useCallback en la función

--- src/hooks/useMovies.js
+++ src/hooks/useMovies.js
@@ -1,4 +1,4 @@
-import { useRef, useState, useMemo } from 'react'
+import { useRef, useState, useMemo, useCallback } from 'react'
 import { searchMovies } from '../services/movies'
 
 export function useMovies ({ search, sort }) {
@@ -7,22 +7,20 @@ export function useMovies ({ search, sort }) {
   const [error, setError] = useState(null)
   const previousSearch = useRef(search)
 
-  const getMovies = useMemo(() => {
-    return async ({ search }) => {
-      if (search === previousSearch.current) return
+  const getMovies = useCallback(async ({ search }) => {
+    if (search === previousSearch.current) return
 
-      try {
-        setLoading(true)
-        setError(null)
-        previousSearch.current = search
-        const newMovies = await searchMovies({ search })
-        setMovies(newMovies)
-      } catch (e) {
-        setError.apply(e.message)
-      } finally {
+    try {
+      setLoading(true)
+      setError(null)
+      previousSearch.current = search
+      const newMovies = await searchMovies({ search })
+      setMovies(newMovies)
+    } catch (e) {
+      setError.apply(e.message)
+    } finally {
       // tanto en el try como en el catch
-        setLoading(false)
-      }
+      setLoading(false)
     }
   }, [])
 
